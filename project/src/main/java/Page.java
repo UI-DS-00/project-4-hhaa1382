c7110ddbd778a1d51d94903afd55d7f68680c575
hhaa1382 <mehranfarhamid82@gmail.com>
2022-12-04 21:54:31 +0330

    search word button add

--- project/src/main/java/Page.java
+++ project/src/main/java/Page.java
@@ -1,8 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
+import java.io.FileNotFoundException;
 
 public class Page extends JFrame {
-    public Page(){
+    private final JTextArea result;
+    private final JTextField text;
+    private final MyTree tree;
+
+    public Page() throws FileNotFoundException {
+        tree=Read.readData();
+
         this.setTitle("Search");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500,500);
@@ -17,27 +24,36 @@ public class Page extends JFrame {
         layout.putConstraint(SpringLayout.NORTH,lblText,50,SpringLayout.NORTH,this.getContentPane());
         layout.putConstraint(SpringLayout.WEST,lblText,50,SpringLayout.WEST,this.getContentPane());
 
-        JTextField text=new JTextField();
+        text=new JTextField();
         text.setPreferredSize(new Dimension(100,30));
         layout.putConstraint(SpringLayout.NORTH,text,50,SpringLayout.NORTH,this.getContentPane());
         layout.putConstraint(SpringLayout.WEST,text,20,SpringLayout.EAST,lblText);
 
-        JButton btnSearch=new JButton("Search");
-        btnSearch.setPreferredSize(new Dimension(80,30));
-        layout.putConstraint(SpringLayout.NORTH,btnSearch,50,SpringLayout.NORTH,this.getContentPane());
-        layout.putConstraint(SpringLayout.WEST,btnSearch,40,SpringLayout.EAST,text);
+        JButton btnSearchWord=new JButton("Search");
+        btnSearchWord.setPreferredSize(new Dimension(80,30));
+        layout.putConstraint(SpringLayout.NORTH,btnSearchWord,50,SpringLayout.NORTH,this.getContentPane());
+        layout.putConstraint(SpringLayout.WEST,btnSearchWord,40,SpringLayout.EAST,text);
+        btnSearchWord.addActionListener(e->{
+            searchButtonListener();
+        });
 
-        JTextArea result=new JTextArea();
+        result=new JTextArea();
         result.setEditable(false);
         result.setPreferredSize(new Dimension(500,200));
         result.setFont(new Font(Font.SANS_SERIF,Font.BOLD,24));
-        layout.putConstraint(SpringLayout.NORTH,result,100,SpringLayout.SOUTH,btnSearch);
+        layout.putConstraint(SpringLayout.NORTH,result,100,SpringLayout.SOUTH,btnSearchWord);
         layout.putConstraint(SpringLayout.WEST,result,0,SpringLayout.WEST,this.getContentPane());
 
         this.add(lblText);
         this.add(text);
-        this.add(btnSearch);
+        this.add(btnSearchWord);
         this.add(result);
         this.setVisible(true);
+    }
+
+    private void searchButtonListener(){
+        String words=text.getText();
+        text.setText("");
+        result.setText(String.valueOf(tree.searchWord(words)));
     }
 }
