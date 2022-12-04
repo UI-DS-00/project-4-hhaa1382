2ed2b71ceec59d341dfcca70777e8d5671aa3b65
hhaa1382 <mehranfarhamid82@gmail.com>
2022-12-04 21:49:39 +0330

    elements add to page frame class

--- project/src/main/java/Main.java
+++ project/src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
+        new Page();
         MyTree values=Read.readData();
 
     }
--- project/src/main/java/Page.java
+++ project/src/main/java/Page.java
@@ -1,2 +1,43 @@
-public class Page {
+import javax.swing.*;
+import java.awt.*;
+
+public class Page extends JFrame {
+    public Page(){
+        this.setTitle("Search");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+
+        SpringLayout layout=new SpringLayout();
+        this.setLayout(layout);
+
+        JLabel lblText=new JLabel("Enter word : ");
+        lblText.setPreferredSize(new Dimension(80,30));
+        layout.putConstraint(SpringLayout.NORTH,lblText,50,SpringLayout.NORTH,this.getContentPane());
+        layout.putConstraint(SpringLayout.WEST,lblText,50,SpringLayout.WEST,this.getContentPane());
+
+        JTextField text=new JTextField();
+        text.setPreferredSize(new Dimension(100,30));
+        layout.putConstraint(SpringLayout.NORTH,text,50,SpringLayout.NORTH,this.getContentPane());
+        layout.putConstraint(SpringLayout.WEST,text,20,SpringLayout.EAST,lblText);
+
+        JButton btnSearch=new JButton("Search");
+        btnSearch.setPreferredSize(new Dimension(80,30));
+        layout.putConstraint(SpringLayout.NORTH,btnSearch,50,SpringLayout.NORTH,this.getContentPane());
+        layout.putConstraint(SpringLayout.WEST,btnSearch,40,SpringLayout.EAST,text);
+
+        JTextArea result=new JTextArea();
+        result.setEditable(false);
+        result.setPreferredSize(new Dimension(500,200));
+        result.setFont(new Font(Font.SANS_SERIF,Font.BOLD,24));
+        layout.putConstraint(SpringLayout.NORTH,result,100,SpringLayout.SOUTH,btnSearch);
+        layout.putConstraint(SpringLayout.WEST,result,0,SpringLayout.WEST,this.getContentPane());
+
+        this.add(lblText);
+        this.add(text);
+        this.add(btnSearch);
+        this.add(result);
+        this.setVisible(true);
+    }
 }
