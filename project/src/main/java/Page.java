df07b1295e740675cf58ebc02fe399ece653fe38
hhaa1382 <mehranfarhamid82@gmail.com>
2022-12-04 22:04:54 +0330

    search list button add

--- project/src/main/java/Page.java
+++ project/src/main/java/Page.java
@@ -22,20 +22,25 @@ public class Page extends JFrame {
         JLabel lblText=new JLabel("Enter word : ");
         lblText.setPreferredSize(new Dimension(80,30));
         layout.putConstraint(SpringLayout.NORTH,lblText,50,SpringLayout.NORTH,this.getContentPane());
-        layout.putConstraint(SpringLayout.WEST,lblText,50,SpringLayout.WEST,this.getContentPane());
+        layout.putConstraint(SpringLayout.WEST,lblText,20,SpringLayout.WEST,this.getContentPane());
 
         text=new JTextField();
         text.setPreferredSize(new Dimension(100,30));
         layout.putConstraint(SpringLayout.NORTH,text,50,SpringLayout.NORTH,this.getContentPane());
         layout.putConstraint(SpringLayout.WEST,text,20,SpringLayout.EAST,lblText);
 
-        JButton btnSearchWord=new JButton("Search");
-        btnSearchWord.setPreferredSize(new Dimension(80,30));
-        layout.putConstraint(SpringLayout.NORTH,btnSearchWord,50,SpringLayout.NORTH,this.getContentPane());
-        layout.putConstraint(SpringLayout.WEST,btnSearchWord,40,SpringLayout.EAST,text);
+        JButton btnSearchWord=new JButton("Search word/text");
+        btnSearchWord.setPreferredSize(new Dimension(150,30));
+        layout.putConstraint(SpringLayout.NORTH,btnSearchWord,25,SpringLayout.NORTH,this.getContentPane());
+        layout.putConstraint(SpringLayout.WEST,btnSearchWord,20,SpringLayout.EAST,text);
         btnSearchWord.addActionListener(e->{
             searchButtonListener();
         });
+
+        JButton btnSearchLists=new JButton("Search files name");
+        btnSearchLists.setPreferredSize(new Dimension(150,30));
+        layout.putConstraint(SpringLayout.NORTH,btnSearchLists,75,SpringLayout.NORTH,this.getContentPane());
+        layout.putConstraint(SpringLayout.WEST,btnSearchLists,20,SpringLayout.EAST,text);
 
         result=new JTextArea();
         result.setEditable(false);
@@ -48,12 +53,15 @@ public class Page extends JFrame {
         this.add(text);
         this.add(btnSearchWord);
         this.add(result);
+        this.add(btnSearchLists);
         this.setVisible(true);
     }
 
     private void searchButtonListener(){
         String words=text.getText();
-        text.setText("");
-        result.setText(String.valueOf(tree.searchWord(words)));
+        if(!words.isBlank()) {
+            text.setText("");
+            result.setText(String.valueOf(tree.searchWord(words)));
+        }
     }
 }
