9449e8c77796890a653049cf2b25d740fb5e9b04
hhaa1382 <mehranfarhamid82@gmail.com>
2022-12-04 19:42:56 +0330

    read all list values add

--- project/src/main/java/Main.java
+++ project/src/main/java/Main.java
@@ -3,42 +3,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-//        String text="would";
-        String text=getTrueString(Read.readWords());
-        System.out.println(text+"\n\n");
-        MyTree tree=new MyTree();
-        tree.addRoot('s');
-        String temp[]=text.split(" ");
-
-        for(int i=0;i<temp.length;i++){
-            tree.addWord(temp[i],"58043");
-        }
-
-        System.out.println(tree.searchWord("guaranteedDoug"));
-        List<String> names=tree.getListsName("GuaranteedDoug");
-        System.out.println(names.size());
-    }
-
-    private static String getTrueString(String text){
-        StringBuilder temp=new StringBuilder();
-
-        for(int i=0;i<text.length();i++){
-            char c=text.charAt(i);
-            if(Character.isLetter(c)){
-                temp.append(c);
-            }
-            else {
-                char tempChar=' ';
-                if (c == '\'') {
-                    tempChar=c;
-                }
-
-                if (i > 0 && Character.isLetter(text.charAt(i - 1))) {
-                    temp.append(tempChar);
-                }
-            }
-        }
-
-        return temp.toString();
+        MyTree values=Read.readData();
+        System.out.println(values.getListsName("the"));
     }
 }
--- project/src/main/java/Read.java
+++ project/src/main/java/Read.java
@@ -3,8 +3,8 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Read {
-    public static String readWords() throws FileNotFoundException {
-        Scanner reader=new Scanner(new File("C:\\Users\\Salam\\Desktop\\Homework\\Data structure\\Tree Project\\data\\EnglishData\\58043"));
+    private static String readWords(File readFile) throws FileNotFoundException {
+        Scanner reader=new Scanner(readFile);
         StringBuilder temp=new StringBuilder();
 
         while (reader.hasNextLine()){
@@ -12,6 +12,58 @@ public class Read {
         }
 
         reader.close();
+        return temp.toString();
+    }
+
+    public static MyTree readData() throws FileNotFoundException {
+        File file=new File("C:\\Users\\Salam\\Desktop\\Homework\\Data structure\\Tree Project\\data\\EnglishData");
+        File[] filesReading=file.listFiles();
+
+        if(filesReading!=null) {
+//            MyTree[] temp=new MyTree[filesReading.length];
+            MyTree temp=new MyTree();
+            temp.addRoot('s');
+
+            for (int i=0;i<2;i++) {
+                String text=getTrueString(readWords(filesReading[i]));
+                setValues(temp,text,filesReading[i].getName());
+            }
+
+            return temp;
+        }
+        else{
+            return null;
+        }
+    }
+
+    private static void setValues(MyTree tree,String text,String fileName){
+        String temp[]=text.split(" ");
+
+        for(int i=0;i<temp.length;i++){
+            tree.addWord(temp[i],fileName);
+        }
+    }
+
+    private static String getTrueString(String text){
+        StringBuilder temp=new StringBuilder();
+
+        for(int i=0;i<text.length();i++){
+            char c=text.charAt(i);
+            if(Character.isLetter(c)){
+                temp.append(c);
+            }
+            else {
+                char tempChar=' ';
+                if (c == '\'') {
+                    tempChar=c;
+                }
+
+                if (i > 0 && Character.isLetter(text.charAt(i - 1))) {
+                    temp.append(tempChar);
+                }
+            }
+        }
+
         return temp.toString();
     }
 }
